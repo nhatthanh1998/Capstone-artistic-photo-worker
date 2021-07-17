e0a4d42f9abf85648bb3737791e57c5de0e2d7bb
nhithanh1998 <30461021+nhatthanh1998@users.noreply.github.com>
2021-07-18 05:51:29 +0700

    update

--- main.py
+++ main.py
@@ -10,36 +10,24 @@ import json
 load_dotenv()
 QUEUE_HOST = os.environ.get("QUEUE_HOST")
 MAIN_SERVER_ENDPOINT = os.environ.get("MAIN_SERVER_ENDPOINT")
-parser = argparse.ArgumentParser()
 
-parser.add_argument("--styleID", type=str, help="styleID that the model belong to", default='', required=True)
-params = parser.parse_args()
-styleID = params.styleID
-
-
-if len(styleID) == 0:
-    raise ValueError("StyleID must be filled!!!")
-else:
-    if __name__ == '__main__':
+if __name__ == '__main__':
+    try:
+        snapshots = {}
+        response = requests.get(f"{MAIN_SERVER_ENDPOINT}/styles/all-snapshots")
+        data = json.loads(response.content.decode('utf-8'))
+        for item in data:
+            snapshots[item["id"]] = item["snapshotPath"]
+        
+        generator_worker = GeneratorWorker(
+            queue_host=QUEUE_HOST,
+            main_server_endpoint=MAIN_SERVER_ENDPOINT,
+            snapshots=snapshots
+        )
+        generator_worker.start_task()
+    except KeyboardInterrupt:
+        print('Interrupted')
         try:
-            response = requests.get(f"{MAIN_SERVER_ENDPOINT}/styles/{styleID}/active-model")
-            data = json.loads(response.content.decode('utf-8'))
-            EXCHANGE_TRANSFER_PHOTO = os.environ.get("EXCHANGE_TRANSFER_PHOTO")
-            EXCHANGE_UPDATE_MODEL = os.environ.get("EXCHANGE_UPDATE_MODEL")
-            routing_key = data.get("routingKey")
-            snapshot_path = data.get("snapshotPath")
-            generator_worker = GeneratorWorker(
-                exchange_transfer_photo_name=EXCHANGE_TRANSFER_PHOTO,
-                exchange_update_model_name=EXCHANGE_UPDATE_MODEL,
-                queue_host=QUEUE_HOST,
-                routing_key=routing_key,
-                snapshot_path=snapshot_path,
-                main_server_endpoint=MAIN_SERVER_ENDPOINT
-            )
-            generator_worker.start_task()
-        except KeyboardInterrupt:
-            print('Interrupted')
-            try:
-                sys.exit(0)
-            except SystemExit:
-                sys.exit(0)
+            sys.exit(0)
+        except SystemExit:
+            sys.exit(0)
--- src/workers/generator.py
+++ src/workers/generator.py
@@ -10,32 +10,35 @@ from datetime import datetime
 
 class GeneratorWorker:
     def __init__(self, queue_host,
-                 exchange_transfer_photo_name,
-                 exchange_update_model_name,
-                 routing_key,
-                 snapshot_path,
+                snapshots,
                  main_server_endpoint):
         self.device = "cpu"
-        # self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.snapshot_path = snapshot_path
-        self.queue_host = queue_host
-        self.exchange_transfer_photo_name = exchange_transfer_photo_name
-        self.exchange_update_model_name = exchange_update_model_name
-        self.routing_key = routing_key
         self.main_server_endpoint = main_server_endpoint
+        self.connection = pika.BlockingConnection(pika.URLParameters(queue_host))
+        self.channel = self.connection.channel()
+        self.snapshots = snapshots
+        # self.snapshots = {
+        #     '8b634cf4-112f-46ce-ac23-ea00f772c152': 'https://artisan-model-snapshots.s3.amazonaws.com/candy/1626159002212?AWSAccessKeyId=AKIAYB7AFYKUNGXO6WOW&Expires=1626619955&Signature=SvjrOJyJVpYmGdaz0WA4Et1UQwg%3D',
+        #     '70c82d96-1cbb-4090-84d3-2b1721af088f': 'https://artisan-model-snapshots.s3.amazonaws.com/rain_princess/1626352835484?AWSAccessKeyId=AKIAYB7AFYKUNGXO6WOW&Expires=1626619955&Signature=VNL3OiP8KFGRJNzzvKDEiyPR0w0%3D',
+        #     'f96ab7b3-fbe3-48dd-a60a-8e729fb80621': 'https://artisan-model-snapshots.s3.amazonaws.com/udnie/1626355468951?AWSAccessKeyId=AKIAYB7AFYKUNGXO6WOW&Expires=1626619955&Signature=TFMrz6yVXk%2FLRmfpt%2BvHNLvO1vk%3D',
+        #     '64b4ef33-e1ce-473b-9d4d-62356b9091e1': 'https://artisan-model-snapshots.s3.amazonaws.com/new_style/1626527641075?AWSAccessKeyId=AKIAYB7AFYKUNGXO6WOW&Expires=1626619955&Signature=ViQFTJyVcT6dCH7N7Hlt5GQNmG0%3D'
+        # }
+        self.weights = {}
+        for key, value in self.snapshots.items():
+            self.weights[key] = torch.hub.load_state_dict_from_url(value, map_location=torch.device(self.device))
         self.generator = Generator().to(self.device)
         self.transform_ = transform()
-        self.generator = load_model(path=self.snapshot_path, generator=self.generator, device=self.device)
-        self.connection = pika.BlockingConnection(pika.URLParameters(self.queue_host))
-        self.channel = self.connection.channel()
+    
+    def load_weight(self, style_id):
+        self.generator.load_state_dict(self.weights[style_id])
 
     def upload_model(self, snapshot_location):
         self.generator = load_model(path=snapshot_location, generator=self.generator, device=self.device)
 
-    def preprocess(self, photo_access_url):
+    def preprocess(self, style_id, photo_access_url):
+        self.load_weight(style_id)
         model_input = Image.open(requests.get(photo_access_url, stream=True).raw)
         model_input = self.transform_(model_input).unsqueeze(0)
-        print(model_input.shape)
         return model_input.to(self.device)
 
     def inference(self, model_input):
@@ -44,16 +47,14 @@ class GeneratorWorker:
     def post_process(self, model_output, image_name, userId, style_id):
         byte_data = transform_tensor_to_bytes(model_output)
         image_location = save_image_to_s3(byte_data, image_name)
-
         endpoint_url = f"{self.main_server_endpoint}/medias/transfer-photo/completed"
-
         payload = {'userId': userId, 'transferPhotoLocation': image_location, 'styleId': style_id}
         requests.post(endpoint_url, data=payload)
         torch.cuda.empty_cache()
 
     def handler(self, ch, method, photo_access_url, userId, image_name, style_id):
         # 1. Preprocess
-        model_input = self.preprocess(photo_access_url=photo_access_url)
+        model_input = self.preprocess(style_id=style_id, photo_access_url=photo_access_url)
 
         # 2. Transform
         model_output = self.inference(model_input=model_input)
@@ -85,22 +86,21 @@ class GeneratorWorker:
         snapshot_location = data['snapshotLocation']
         self.upload_model(snapshot_location)
 
-    def declare_transfer_photo_workflow(self):
-        self.channel.queue_declare(self.routing_key, durable=True)
-        self.channel.exchange_declare(exchange=self.exchange_transfer_photo_name, exchange_type='direct')
-        self.channel.queue_bind(exchange=self.exchange_transfer_photo_name, queue=self.routing_key,
-                                routing_key=self.routing_key)
-        self.channel.basic_consume(queue=self.routing_key, on_message_callback=self.process_transfer_photo_task)
-        print(f' [*] Waiting for messages at exchange {self.exchange_transfer_photo_name} routing Key: {self.routing_key}. To exit press CTRL+C')
+    def init_transfer_photo_queue(self):
+        self.channel.queue_declare("TRANSFER_PHOTO_QUEUE", durable=True)
+        self.channel.exchange_declare("TRANSFER_PHOTO_EXCHANGE", exchange_type='direct')
+        self.channel.queue_bind(exchange="TRANSFER_PHOTO_EXCHANGE", queue="TRANSFER_PHOTO_QUEUE", routing_key="")
+        self.channel.basic_consume(queue="TRANSFER_PHOTO_QUEUE", on_message_callback=self.process_transfer_photo_task)
+        print(f' [*] Waiting for messages at TRANSFER PHOTO EXCHANGE. To exit press CTRL+C')
 
     def declare_update_model_workflow(self):
         rs = self.channel.queue_declare(queue='', exclusive=True)
         queue_name = rs.method.queue
-        self.channel.exchange_declare(exchange=self.exchange_update_model_name, exchange_type='fanout')
-        self.channel.queue_bind(exchange=self.exchange_update_model_name, queue=queue_name, routing_key=self.routing_key)
+        self.channel.exchange_declare(exchange="UPDATE_WEIGHT_EXCHANGE", exchange_type='fanout')
+        self.channel.queue_bind(exchange="UPDATE_WEIGHT_EXCHANGE", queue=queue_name)
         self.channel.basic_consume(queue=queue_name, on_message_callback=self.process_update_model_task)
 
     def start_task(self):
-        self.declare_transfer_photo_workflow()
+        self.init_transfer_photo_queue()
         self.declare_update_model_workflow()
         self.channel.start_consuming()
